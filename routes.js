53c15f69df8f0e678bdbd8209e77b37d54ecac2c
JohnnyKB <j.kutnob@gmail.com>
2017-10-07 02:40:51 +0200

    sss

--- routes.js
+++ routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const request = require('request');
 const router = express.Router();
 const IG_URL = 'https://api.instagram.com/v1/';
-const IG_ACCESS_TOKEN = '1119844.93ea1cf.7186f364d56b48d1a214806f419a37d3';
 router.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html', { message: 'hello' });
 });
